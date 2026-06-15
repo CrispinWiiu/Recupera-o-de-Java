f06faba0f98828d8c0115a1ded1ded30258483b9
Crispin <enzo.r.crispin@gmail.com>
2026-06-15 14:56:29 -0300

    Update Veiculo.java

--- projetos_java/projeto2/Veiculo.java
+++ projetos_java/projeto2/Veiculo.java
@@ -1,34 +1,45 @@
-public class Veiculo {
+public class Veiculo 
+{
     private String marca;
     private String modelo;
     private String cor;
     private int anoFabricacao;
 
-    public Veiculo() {
+    public Veiculo() 
+    {
         this.marca = "";
         this.modelo = "";
         this.cor = "";
         this.anoFabricacao = 0;
     }
 
-    public Veiculo(String marca, String modelo, String cor, int anoFabricacao) {
+    public Veiculo(String marca, String modelo, String cor, int anoFabricacao) 
+    {
         this.marca = marca;
         this.modelo = modelo;
         this.cor = cor;
         this.anoFabricacao = anoFabricacao;
     }
 
-    public String getMarca() { return marca; }
-    public void setMarca(String marca) { this.marca = marca; }
+    public String getMarca() 
+    { return marca; }
+    public void setMarca(String marca) 
+    { this.marca = marca; }
 
-    public String getModelo() { return modelo; }
-    public void setModelo(String modelo) { this.modelo = modelo; }
+    public String getModelo() 
+    { return modelo; }
+    public void setModelo(String modelo) 
+    { this.modelo = modelo; }
 
-    public String getCor() { return cor; }
-    public void setCor(String cor) { this.cor = cor; }
+    public String getCor() 
+    { return cor; }
+    public void setCor(String cor) 
+    { this.cor = cor; }
 
-    public int getAnoFabricacao() { return anoFabricacao; }
-    public void setAnoFabricacao(int anoFabricacao) { this.anoFabricacao = anoFabricacao; }
+    public int getAnoFabricacao() 
+    { return anoFabricacao; }
+    public void setAnoFabricacao(int anoFabricacao) 
+    { this.anoFabricacao = anoFabricacao; }
 
     @Override
     public String toString() {
