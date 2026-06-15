9226b67fbcae4868c94311fa87e2e5806142dcc2
Crispin <enzo.r.crispin@gmail.com>
2026-06-15 14:45:52 -0300

    Update Projeto.java
    Formatted getter and setter methods for better readability.

--- projetos_java/projeto1/Projeto.java
+++ projetos_java/projeto1/Projeto.java
@@ -1,18 +1,27 @@
-public class Projeto {
+public class Projeto 
+{
     private int codigo;
     private String titulo;
     private int diasDuracao;
     private String descricao;
 
-    public int getCodigo() { return codigo; }
-    public void setCodigo(int codigo) { this.codigo = codigo; }
+    public int getCodigo() 
+    { return codigo; }
+    public void setCodigo(int codigo) 
+    { this.codigo = codigo; }
 
-    public String getTitulo() { return titulo; }
-    public void setTitulo(String titulo) { this.titulo = titulo; }
+    public String getTitulo() 
+    { return titulo; }
+    public void setTitulo(String titulo) 
+    { this.titulo = titulo; }
 
-    public int getDiasDuracao() { return diasDuracao; }
-    public void setDiasDuracao(int diasDuracao) { this.diasDuracao = diasDuracao; }
+    public int getDiasDuracao() 
+    { return diasDuracao; }
+    public void setDiasDuracao(int diasDuracao) 
+    { this.diasDuracao = diasDuracao; }
 
-    public String getDescricao() { return descricao; }
-    public void setDescricao(String descricao) { this.descricao = descricao; }
+    public String getDescricao() 
+    { return descricao; }
+    public void setDescricao(String descricao) 
+    { this.descricao = descricao; }
 }
