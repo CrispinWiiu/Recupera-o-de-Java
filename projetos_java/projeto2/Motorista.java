c091382b298c63dc1f40155c4f462666505a113a
Crispin <enzo.r.crispin@gmail.com>
2026-06-15 14:55:18 -0300

    Update Motorista.java

--- projetos_java/projeto2/Motorista.java
+++ projetos_java/projeto2/Motorista.java
@@ -1,22 +1,29 @@
-public class Motorista {
+public class Motorista 
+{
     private String nome;
     private String numHabilitacao;
 
-    public Motorista() {
+    public Motorista() 
+    {
         this.nome = "";
         this.numHabilitacao = "";
     }
 
-    public Motorista(String nome, String numHabilitacao) {
+    public Motorista(String nome, String numHabilitacao) 
+    {
         this.nome = nome;
         this.numHabilitacao = numHabilitacao;
     }
 
-    public String getNome() { return nome; }
-    public void setNome(String nome) { this.nome = nome; }
+    public String getNome() 
+    { return nome; }
+    public void setNome(String nome) 
+    { this.nome = nome; }
 
-    public String getNumHabilitacao() { return numHabilitacao; }
-    public void setNumHabilitacao(String numHabilitacao) { this.numHabilitacao = numHabilitacao; }
+    public String getNumHabilitacao() 
+    { return numHabilitacao; }
+    public void setNumHabilitacao(String numHabilitacao) 
+    { this.numHabilitacao = numHabilitacao; }
 
     @Override
     public String toString() {
