1071960b7c7fbf1c861c4e61e9c350fd518c258c
Crispin <enzo.r.crispin@gmail.com>
2026-06-15 14:43:07 -0300

    Update EntregaCarro.java

--- projetos_java/projeto4/EntregaCarro.java
+++ projetos_java/projeto4/EntregaCarro.java
@@ -1,16 +1,20 @@
-public class EntregaCarro extends Entrega implements Rastreavel {
+public class EntregaCarro extends Entrega implements Rastreavel 
+{
 
-    public EntregaCarro(double distancia) {
+    public EntregaCarro(double distancia) 
+    {
         super(distancia);
     }
 
     @Override
-    public double calcularCusto() {
+    public double calcularCusto() 
+    {
         return distancia * 2.50;
     }
 
     @Override
-    public void rastrear() {
+    public void rastrear() 
+    {
         System.out.println("Rastreamento via carro: acompanhe a localização pelo GPS do veículo.");
     }
 }
