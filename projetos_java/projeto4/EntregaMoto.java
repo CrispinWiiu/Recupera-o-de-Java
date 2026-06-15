5b0ae33241f27a695571fac2538b2c809bc42838
Crispin <enzo.r.crispin@gmail.com>
2026-06-15 14:41:30 -0300

    Update EntregaMoto.java

--- projetos_java/projeto4/EntregaMoto.java
+++ projetos_java/projeto4/EntregaMoto.java
@@ -1,16 +1,20 @@
-public class EntregaMoto extends Entrega implements Rastreavel {
+public class EntregaMoto extends Entrega implements Rastreavel 
+{
 
-    public EntregaMoto(double distancia) {
+    public EntregaMoto(double distancia) 
+    {
         super(distancia);
     }
 
     @Override
-    public double calcularCusto() {
+    public double calcularCusto() 
+    {
         return distancia * 1.50;
     }
 
     @Override
-    public void rastrear() {
+    public void rastrear() 
+    {
         System.out.println("Rastreamento via moto: entrega rápida, acompanhe pelo aplicativo do motoboy.");
     }
 }
