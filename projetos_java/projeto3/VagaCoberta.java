a0b30af97b1fc129c370bd63a008297ebad88896
Crispin <enzo.r.crispin@gmail.com>
2026-06-15 14:52:50 -0300

    Update VagaCoberta.java

--- projetos_java/projeto3/VagaCoberta.java
+++ projetos_java/projeto3/VagaCoberta.java
@@ -1,17 +1,21 @@
-public class VagaCoberta extends Estacionamento {
+public class VagaCoberta extends Estacionamento 
+{
     private double valorCobertura;
 
-    public VagaCoberta(double valor, double valorCobertura) {
+    public VagaCoberta(double valor, double valorCobertura) 
+    {
         super(valor);
         this.valorCobertura = valorCobertura;
     }
 
-    public double getValorTotal() {
+    public double getValorTotal() 
+    {
         return valor + valorCobertura;
     }
 
     @Override
-    public void imprimeValor() {
+    public void imprimeValor() 
+    {
         System.out.println("Vaga Coberta - Valor base: R$ " + String.format("%.2f", valor) +
                 " + Cobertura: R$ " + String.format("%.2f", valorCobertura) +
                 " = Total: R$ " + String.format("%.2f", getValorTotal()));
