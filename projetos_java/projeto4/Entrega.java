f35ceab64a736972cd2365f2f5adf20a9aa90ac9
Crispin <enzo.r.crispin@gmail.com>
2026-06-15 14:47:59 -0300

    Update Entrega.java

--- projetos_java/projeto4/Entrega.java
+++ projetos_java/projeto4/Entrega.java
@@ -1,17 +1,21 @@
-public abstract class Entrega {
+public abstract class Entrega 
+{
     protected double distancia;
 
-    public Entrega(double distancia) {
+    public Entrega(double distancia) 
+    {
         this.distancia = distancia;
     }
 
-    public double getDistancia() {
+    public double getDistancia() 
+    {
         return distancia;
     }
 
     public abstract double calcularCusto();
 
-    public void exibirInformacoes() {
+    public void exibirInformacoes() 
+    {
         System.out.println("Distância: " + distancia + " km");
     }
 }
